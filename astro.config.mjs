d7d0721e3eea9c6c459cc61322a6783d6baa5ff0
viotalJiplk <viotal.varecha@outlook.com>
2026-06-06 12:58:12 +0200

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    "server": {
-        "port": 4321,
-        "host": "127.0.0.1"
-    }
+  "server": {
+      "port": 4321,
+      "host": "127.0.0.1"
+  },
+  site: 'https://gttournament.cz',
+  integrations: [sitemap()]
 });
